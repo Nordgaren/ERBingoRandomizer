de8350731eff05ea0862b39c5a7e5c4c78a775db
srgntpepper <chrismdavis2009@gmail.com>
2024-07-25 20:36:12 -0700

    WOP development for animation(GIF)

---
+++ src/ERBingoRandomizer/Converter/IsLastItemGIFConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Randomizer.Converter
+{
+    public class IsLastItemGIFConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is int currentIndex && values[1] is int itemCount)
+            {
+                return currentIndex == itemCount - 1 ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
